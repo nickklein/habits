6ebaf6cdc66d1e7e5a069e2e3c97156cebf4aa33
Nick Klein <1294247+nickklein@users.noreply.github.com>
2025-04-09 17:01:07 -0700

    fix event listener parameters

--- src/Services/HabitInsightService.php
+++ src/Services/HabitInsightService.php
@@ -255,7 +255,7 @@ class HabitInsightService
         $habitTime->end_time = date('Y-m-d H:i:s');
         // difference between start_time and end_time in minutes using Carbon/Carbon
         $habitTime->duration = Carbon::parse($habitTime->start_time)->diffInSeconds($habitTime->end_time);
-        event(new HabitEndedEvent($habitTime));
+        event(new HabitEndedEvent($userId, $habitTime));
 
         return $habitTime->save();
     }
